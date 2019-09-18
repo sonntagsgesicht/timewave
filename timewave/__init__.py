898e8791e65ad4ddd0473a9880090bfdf9fbad04
sonntagsgesicht <sonntagsgesicht@icloud.com>
2019-09-18 15:03:05 +0200

    upgrade to version 0.6

--- timewave/__init__.py
+++ timewave/__init__.py
@@ -5,7 +5,7 @@
 # timewave, a stochastic process evolution simulation engine in python.
 # 
 # Author:   sonntagsgesicht, based on a fork of Deutsche Postbank [pbrisk]
-# Version:  0.5, copyright Wednesday, 18 September 2019
+# Version:  0.6, copyright Wednesday, 18 September 2019
 # Website:  https://github.com/sonntagsgesicht/timewave
 # License:  Apache License 2.0 (see LICENSE file)
 
@@ -18,7 +18,7 @@ __email__ = 'sonntagsgesicht@icloud.com'
 __url__ = 'https://github.com/sonntagsgesicht/' + __name__
 
 __date__ = 'Wednesday, 18 September 2019'
-__version__ = '0.5'
+__version__ = '0.6'
 __dev_status__ = '4 - Beta'
 
 __dependencies__ = ('dill', 'numpy', 'scipy')
--- timewave/stochasticconsumer.py
+++ timewave/stochasticconsumer.py
@@ -5,7 +5,7 @@
 # timewave, a stochastic process evolution simulation engine in python.
 # 
 # Author:   sonntagsgesicht, based on a fork of Deutsche Postbank [pbrisk]
-# Version:  0.5, copyright Wednesday, 18 September 2019
+# Version:  0.6, copyright Wednesday, 18 September 2019
 # Website:  https://github.com/sonntagsgesicht/timewave
 # License:  Apache License 2.0 (see LICENSE file)
 
